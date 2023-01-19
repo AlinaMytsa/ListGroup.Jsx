224456878001453103927ce5fe484fd7ca6cc2f7
AlinaMytsa <act.mitsa@gmail.com>
2023-01-19 20:15:06 +0200

    update

--- src/components/ListGroup.jsx
+++ src/components/ListGroup.jsx
@@ -2,41 +2,18 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 class ListGroup extends React.Component{
-//   renderingKids = kids => {
-//     return kids.map((item, key) => <li key={key} className="list-group-item">{item}</li>
-//     )
-//   }
-//
-//   render() {
-//     const {children} = this.props;
-//
-//     return(
-//       <ul className="list-group">
-//         {this.renderingKids(children)}
-//       </ul>
-//     )
-//   }
-  //   -------------
+  renderingKids = kids => {
+    return kids.map((item, key) => <li key={key} className="list-group-item">{item}</li>
+    )
+  }
 
   render() {
     const {children} = this.props;
 
-    function* getKey(){
-      let i = 0;
-      while (true){
-        yield ++ i
-      }
-    }
-    const kidsKey = getKey();
-
     return(
-      <ul className="list-group">{
-        children.map(item => {
-          return(
-            <li className="list-group-item" key={kidsKey.next().value}>{item}</li>
-          )
-        })
-      }</ul>
+      <ul className="list-group">
+        {this.renderingKids(children)}
+      </ul>
     )
   }
 
